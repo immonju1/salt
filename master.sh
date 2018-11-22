0aee935d0fe7b8e3afc0323b134a3074b42e84ad
Juha Immonen <jmi@kolumbus.fi>
2018-11-22 14:33:24 +0000

    fix master.sh

--- master.sh
+++ master.sh
@@ -1,7 +1,10 @@
 # setup salt master
 # wget https://raw.githubusercontent.com/immonju1/salt/master/master.sh
+# chmod g+x master.sh
+# sudo ./master.sh
 
 setxkbmap fi
+echo "Updating packages and install salt and git..."
 apt-get update -qq >> /dev/null
 apt-get install git salt-master -y -qq >> /dev/null
 
@@ -9,13 +12,13 @@ apt-get install git salt-master -y -qq >> /dev/null
 if [ ! -d "/srv/" ]; then
 mkdir /srv/
 fi
-
+echo "Cloning Git..."
 cd /srv
 
 git clone https://github.com/immonju1/salt.git
 
 mkdir /srv/pillar
-
+echo "Copying pillars..."
 cp -R /srv/salt/srvpillar/* /srv/pillar
 
 
